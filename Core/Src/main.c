2ac5ce23cd018fe81867c455c9af3e6a8a957329
404elf <1543738958@qq.com>
2026-04-06 13:37:27 +0800

    2-配置DAC

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,8 +18,11 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dac.h"
+#include "dma.h"
 #include "rtc.h"
 #include "spi.h"
+#include "tim.h"
 #include "usart.h"
 #include "usb_device.h"
 #include "gpio.h"
@@ -91,11 +94,14 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_FSMC_Init();
   MX_RTC_Init();
   MX_SPI3_Init();
   MX_USART1_UART_Init();
   MX_USB_DEVICE_Init();
+  MX_DAC_Init();
+  MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
